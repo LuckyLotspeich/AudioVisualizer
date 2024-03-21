3071eb03e083a2b1fb0896daa52b9fc3c8af77a4
LuckyLotspeich <nicklotspeich@u.boisestate.edu>
2024-03-21 14:31:56 -0600

    Started on Synth
    Got Initial Beats working and instrument changing functionality working

--- Assets/Scripts/SequencerInstrument.cs
+++ Assets/Scripts/SequencerInstrument.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+// using System.IO.Directory;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
-using TMPro;
 using UnityEngine.VFX;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
-using DG.Tweening;
+using TMPro;
+using DG.Tweening; 
 
 public class SequencerInstrument : MonoBehaviour
 {
@@ -19,14 +21,38 @@ public class SequencerInstrument : MonoBehaviour
 
     [Header("Note States")]
     public List<NoteData> notesStates = new List<NoteData>();
-    public string noteTag; // The tag assigned in the inspector
 
-    [Header("Test")]
-    public TextMeshProUGUI outputText;
+    [Header("Note")]
+    public AudioSource audioSource;
+    public List<AudioClip> instrumentClips = new List<AudioClip>();
+    // public AudioClip selectedInstrumentClip; 
+    
+    [Header("UI")]
+    public TextMeshProUGUI instrumentText;
+    public Slider instrumentSlider;
+    public string noteTag;
 
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
+
+        // Automatically adds all the audio clips from the AudioClips folder into the list
+        string[] audioFilesPaths = AssetDatabase.FindAssets("t:AudioClip", new[] {"Assets/Audio/AudioClips"});
+
+        foreach (var audioFilePath in audioFilesPaths) {
+            string path = AssetDatabase.GUIDToAssetPath(audioFilePath);
+            AudioClip audioFile = AssetDatabase.LoadAssetAtPath<AudioClip>(path);
+            if (audioFile != null) {
+                instrumentClips.Add(audioFile);
+            }
+            
+        }
+
+        // Adjusts the slider and updates it
+        instrumentSlider.maxValue = instrumentClips.Count - 1;
+        UpdateSelectedInstrument();
+
         foreach (Transform child in transform) {
             if (child.CompareTag(noteTag)) {
                 GameObject note = child.gameObject;
@@ -35,48 +61,35 @@ public class SequencerInstrument : MonoBehaviour
                 notesStates.Add(noteData);
             }
         }
+        
     }
 
     void Update() {
         // Touch Functionality
         RaycastHit? touchPosition = SynthesizerManager.HandleTouchInput();
 
-        if (touchPosition != null)
-        {
+        if (touchPosition != null) {
             RaycastHit hit = touchPosition.Value;
 
-            if (hit.collider.gameObject.CompareTag(noteTag))
-            {
+            if (hit.collider.gameObject.CompareTag(noteTag)) {
                 GameObject note = hit.collider.gameObject;
 
                 NoteData noteData = GetNoteData(note);
 
-                if (noteData != null)
-                {
-                    // Get the state of the hit note
-                    bool hitnoteState = noteData.noteState;
-                    hitnoteState = !hitnoteState;
-
-                    // Updates the list with the new value
-                    noteData.noteState = hitnoteState;
+                if (noteData != null) {
+                    noteData.noteState = !noteData.noteState;
 
                     Renderer renderer = note.GetComponent<Renderer>();
-                    if (renderer != null)
-                    {
-                        if (hitnoteState)
-                        {
+                    if (renderer != null) {
+                        if (noteData.noteState) {
                             renderer.material = toggleOnMat;
                             Debug.Log("Is On");
                         }
-                        else
-                        {
+                        else {
                             renderer.material = toggleOffMat;
                             Debug.Log("Is Off");
                         }
                     }
-                }
-                else {
-                    Debug.LogWarning("NoteData not found for the note GameObject.");
                 }
             }
         }
@@ -88,34 +101,62 @@ public class SequencerInstrument : MonoBehaviour
                 return noteData;
             }
         }
-        return null;
+        Debug.Log("returning null");
+        return null;        
     }
 
-    public void ResetArray() {
-        Debug.Log("Reset The Panels");
-    }
-
-    public void PrintActiveNotes() {
-        string activeNotes = "Active Notes:\n";
+    public List<bool> GetActiveNoteStates() {
+        List<bool> activeNoteStates = new List<bool>();
 
         foreach (NoteData noteData in notesStates) {
-            if (noteData.noteState) {
-                activeNotes += noteData.note.name + "\n";
+            activeNoteStates.Add(noteData.noteState);
+        }
+        return activeNoteStates;
+    }
+
+    public void OnInstrumentSliderChanged() {
+        UpdateSelectedInstrument();
+    }
+
+    public void UpdateSelectedInstrument() {
+        int selectedInstrumentIndex = (int)instrumentSlider.value;
+
+        if (selectedInstrumentIndex >=0 && selectedInstrumentIndex < instrumentClips.Count) {
+            // selectedInstrumentClip = instrumentClips[selectedInstrumentIndex];
+            string selectedInstrumentName = instrumentClips[selectedInstrumentIndex].name;
+            if (instrumentClips[selectedInstrumentIndex] != null) {
+                audioSource.clip = instrumentClips[selectedInstrumentIndex];
+                instrumentText.text = selectedInstrumentName; 
+            }
+            else {
+                Debug.LogWarning("No audio clip assigned to the selected instrument.");
             }
         }
-
-        outputText.text = activeNotes;
     }
-}
 
+    public void PlaySound() {
+        audioSource.PlayOneShot(instrumentClips[(int)instrumentSlider.value]);
+    }
+
+    public void ResetNotes() {
+        Debug.Log("Reset The Panels");
+        foreach (NoteData noteData in notesStates) {
+            noteData.noteState = false;
+            Renderer renderer = noteData.note.GetComponent<Renderer>();
+            if (renderer != null) {
+                renderer.material = toggleOffMat; // Reset material to default off state
+            }
+        }
+    }
+
+}
 
 public class NoteData
 {
     public GameObject note;
     public bool noteState;
 
-    public NoteData(GameObject note, bool noteState)
-    {
+    public NoteData(GameObject note, bool noteState) {
         this.note = note;
         this.noteState = noteState;
     }
--- Assets/Scripts/SynthesizerManager.cs
+++ Assets/Scripts/SynthesizerManager.cs
@@ -1,27 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.VFX;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using TMPro;
+using DG.Tweening; 
 
 public class SynthesizerManager : MonoBehaviour
 {
+    public static SynthesizerManager Instance;
+    public SequencerInstrument SequencerInstrument;
     public Camera cam;
 
-    public float bpm = 120f;
+    [Header("Test")]
+    public TextMeshProUGUI screenSizeText;
+
+    [Header("UI")]
+    public TextMeshProUGUI bpmText;
+    public Slider bpmSlider;
+
+    [Header("Instruments")]
+    public List<SequencerInstrument> instruments = new List<SequencerInstrument>();
+    public float bpm = 60f;
     // Used to track the time ingame
-    public float beatInterval;
-    public float beatTimer;
-    public float test1;
+    private float beatInterval;
+    private float beatTimer;
+    private int currentBeatIndex = 0;
+
+    private void Awake()
+    {
+        if (Instance == null) {
+            Instance = this;
+            Debug.Log("Instance is this");
+        }
+        else {
+            Destroy(gameObject);
+        }    
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Test Values
+        screenSizeText.text = "Screen Width: " + Screen.width.ToString();
+
+        // Automatically adds the sequencers in scene to the manager
+        SequencerInstrument[] sequencerInstruments = GetComponentsInChildren<SequencerInstrument>();
+        instruments.AddRange(sequencerInstruments);
+
+        GameObject sequencerManagerObject = GameObject.Find("SequencerManager");
+
+        if (sequencerManagerObject != null) {
+            Transform sequencerManagerTransform = sequencerManagerObject.transform;
+
+            foreach (Transform child in sequencerManagerTransform) {
+
+                if (child.name == "Sequencer") {    // can instantiate prefabs with this so the name does match
+                    SequencerInstrument sequencerInstrument = child.GetComponent<SequencerInstrument>();
+
+                    if (sequencerInstrument != null) {
+                        instruments.Add(sequencerInstrument);
+                    }
+                }
+            }
+        }
+
+
+
+        CalculateBeatInterval();
     }
 
     // Update is called once per frame
     void Update() {
+
         HandleTouchInput();
-        // If keys are pressed.
+
         if(Input.GetKeyUp(KeyCode.Q)) {
             QuitApplication();
         }
@@ -29,8 +85,10 @@ public class SynthesizerManager : MonoBehaviour
         beatTimer += Time.deltaTime;
         if (beatTimer >= beatInterval)
         {
+            // This is where the beat occurs! Ba-dum!
             beatTimer -= beatInterval;
-            // Activate my instument sounds here
+            ActivateNotes();
+            currentBeatIndex++;
         }
     }
 
@@ -46,6 +104,37 @@ public class SynthesizerManager : MonoBehaviour
             }
         }
         return null;
+    }
+
+    public void CalculateBeatInterval() {
+        beatInterval = 60f / bpm;
+    }
+
+    public void UpdateBPM() {
+        int newBPM = (int)bpmSlider.value;
+        bpm = newBPM;
+        CalculateBeatInterval();
+        bpmText.text = "BPM: " + bpm;
+    }
+
+    public void ActivateNotes() {
+        foreach (SequencerInstrument instrument in instruments) {
+            List<bool> activeNoteStates = instrument.GetActiveNoteStates();
+
+            if (currentBeatIndex >= activeNoteStates.Count) {
+                currentBeatIndex = 0;
+            }
+            
+            if (currentBeatIndex >= 0 && currentBeatIndex < activeNoteStates.Count) {   
+                if (activeNoteStates[currentBeatIndex]) {
+                    instrument.PlaySound();
+                    // Debug.Log("Note activated: Instrument " + instrument.name + ", Beat Index " + currentBeatIndex);
+                }
+            }
+            else {
+                Debug.LogWarning("Beat index out of range for instrument " + instrument.name);
+            }
+        }
     }
 
     public void QuitApplication() {
